a029b7e4f803cd132aae5cba1d8107623b5846d7
CortezDeveloper <josecortezsoftware@gmail.com>
2023-09-01 03:08:11 +0200

    Project3Final

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./../style/App.css"
 import Bannerimg from "./../assets/images/fnmbanner.jpg"
+// import Banner2 from "./../assets/images/banner2.jpg"
 
 function Banner() {
   return (
     <div>
       <img className="img-banner" src={Bannerimg} />    
+      
     </div>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,17 +2,24 @@ import React from 'react'
 import NavBar from './../components/NavBar'
 import Footer from './../components/Footer'
 import Bannerimg from "./../assets/images/fnmbanner.jpg"
+// import Banner2 from "./../assets/images/banner2.png"
+import Banner2 from "./../assets/images/bannercinco.jpg"
+import "./../style/Homepage.css"
 // import Banner from './../components/Banner'
 
 
 
 function HomePage() {
   return (
-    <div>
+    <div className="homepage">
       <NavBar />
       {/* <Banner /> */}
-      <img className="img-banner" src={Bannerimg} />
-      <Footer />
+      <div className="image-container">
+        <img className="img-banner" src={Bannerimg} />
+        {/* <img className="img-banner2" src={Banner2} /> */}
+        <img className="img-banner" src={Banner2} />
+      </div>
+     <Footer />
     </div>
  
   )
